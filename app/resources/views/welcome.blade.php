594df8ea9177000c3823a55e13886f75716d46b6
dega <degavv@gmail.com>
2025-07-21 10:41:50 +0300

    view add link to /task

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <div><a href="/task">Task List</a></div>
             </div>
         </div>
     </body>
