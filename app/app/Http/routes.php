c4757edd6794eefeb3725fb33998fe13573c460b
dega <degavv@gmail.com>
2025-07-23 12:21:51 +0300

    routes update routes added

--- app/app/Http/routes.php
+++ app/app/Http/routes.php
@@ -13,6 +13,7 @@
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use App\Services\TaskService;
 
 Route::get('/', function () {
     return view('welcome');
@@ -31,20 +32,37 @@ Route::group(['prefix' => 'task'], function () {
     })->name('task.create');
 
     Route::post('/', function (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-        ]);
-        if ($validator->fails()) {
+        list($newTask, $validator) = TaskService::validate($request);
+
+        if ($validator) {
             return redirect()
                 ->route('task.create')
                 ->withInput()
                 ->withErrors($validator);
         }
-        $task = new Task();
-        $task->name = $request->name;
-        $task->save();
+
         return redirect()->route('task.index');
     })->name('task.store');
+
+    Route::get('/edit/{task}', function (Task $task) {
+        return view('task.edit', [
+            'task_id' => $task->id,
+            'task_name' => $task->name,
+        ]);
+    })->name('task.edit');
+
+    Route::patch('/{task}', function (Task $task, Request $request) {
+        list($savedTask, $validator) = TaskService::validate($request, $task);
+
+        if ($validator) {
+            return redirect()
+                ->route('task.edit', $task->id)
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        return redirect()->route('task.index');
+    })->name('task.update');
 
     Route::delete('/{task}', function (Task $task) {
         $task->delete();
